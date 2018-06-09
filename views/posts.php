b27c8138bd89c0ca7a2313010ff48929e194746e
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-06-09 12:23:25 +0200

    Formatting

--- views/posts.php
+++ views/posts.php
@@ -1,54 +1,61 @@
-   <!--  <form>
+<!--  <form>
         <input type="search" name="tag" placeholder="Find posts by tags">
         <input type="submit" value="Search">
     </form> -->
 
-    <main class="center_text"role="main">
-        <div class="image"></div>
+<main class="center_text" role="main">
+    <div class="image"></div>
 
-        <section>
+    <section>
         <nav id="primary_nav_wrap">
-        <h3>Categories</h3>
-        <ul>
-            
-        <?php
-            $categories = $params['categories'];
-            foreach ($categories as $i => $category) :
-                $name=$category->getName();
-                $id=$category->getId();
+            <h3>Categories</h3>
+            <ul>
 
-                echo "<li><a href='/categories/$id/posts/1'>$name</a></li>";
-            endforeach;
-            ?>
-        </ul>
-    </nav>
+                <?php
+$categories = $params['categories'];
+foreach ($categories as $i => $category):
+    $name = $category->getName();
+    $id = $category->getId();
+
+    echo "<li><a href='/categories/$id/posts/1'>$name</a></li>";
+endforeach;
+?>
+            </ul>
+        </nav>
     </section>
 
     <section>
 
         <?php
-        $posts = $params['posts'];
-        foreach ($posts as $i => $post) :
-        ?>
+$posts = $params['posts'];
+foreach ($posts as $i => $post):
+?>
 
-             <article class="container post_img">
-                <h1><?php echo $post->getTitle(); ?></h1>
-                <p><?php echo $post->getCategory(); ?></p>
-                <h2><?php echo $post->getDate(); ?></h2>
-                <p><?php echo $post->getBody(); ?></p>
+            <article class="container post_img">
+                <h1>
+                    <?php echo $post->getTitle(); ?>
+                </h1>
+                <p>
+                    <?php echo $post->getCategory(); ?>
+                </p>
+                <h2>
+                    <?php echo $post->getDate(); ?>
+                </h2>
+                <p>
+                    <?php echo $post->getBody(); ?>
+                </p>
 
                 <?php if (!empty($post->getImageUrl())) {
-            $url = $post->getImageUrl();
-            echo "<img src='$url' width='800' height='500'>";
-        } ?>
+    $url = $post->getImageUrl();
+    echo "<img src='$url' width='800' height='500'>";
+}?>
 
                 <?php
-                $tagmodel = $params['tagModel'];
-                $tags = $tagmodel->getTagsOfPost($post->getId());
+$tagmodel = $params['tagModel'];
+$tags = $tagmodel->getTagsOfPost($post->getId());
 
-
-                // variable_att_spara_return_värde_i = $objektet_name->funktionens_name($paramterns_namn);
-                ?>
+// variable_att_spara_return_värde_i = $objektet_name->funktionens_name($paramterns_namn);
+?>
 
 
             </article>
@@ -58,27 +65,25 @@
                 <i class="fa fa-snapchat-ghost" aria-hidden="true"></i>
                 <i class="fa fa-envelope" aria-hidden="true"></i>
             </aside>
-        <?php endforeach; ?>
+            <?php endforeach;?>
 
-        </section>
+    </section>
 
-    </main>
+</main>
 </body>
 
 <footer>
 
-<?php if ($params['currentPage'] > 1) {
-                    $previousPageNumber = $params['currentPage'] - 1;
+    <?php if ($params['currentPage'] > 1) {
+    $previousPageNumber = $params['currentPage'] - 1;
 
-                    echo "<a href='/posts/$previousPageNumber' class='previous_button'>&laquo; Previous</a>";
-                } ?>
+    echo "<a href='/posts/$previousPageNumber' class='previous_button'>&laquo; Previous</a>";
+}?>
 
-<?php if (!$params['isLastPage']) {
-                    $nextPageNumber = $params['currentPage'] + 1;
+    <?php if (!$params['isLastPage']) {
+    $nextPageNumber = $params['currentPage'] + 1;
 
-                    echo "<a href='/posts/$nextPageNumber' class='next_button'> Next&raquo;</a>";
-                } ?>
+    echo "<a href='/posts/$nextPageNumber' class='next_button'> Next&raquo;</a>";
+}?>
 
 </footer>
-
-
